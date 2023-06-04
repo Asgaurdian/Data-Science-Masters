43e3d1dd1de10f836239aa6be3874a5d13b03701
Asgaurdian <127683864+Asgaurdian@users.noreply.github.com>
2023-06-04 21:42:43 +0530

    Add files via upload
    In this piece there are examples of loops.

---
+++ Day2-loops.py
@@ -0,0 +1,23 @@
+#while loop
+# total_amount = 1000
+# while total_amount != 0:
+#     print(total_amount)
+#     total_amount = total_amount - 100
+# else:
+#     print("Low Balance")
+    
+
+#Pyramid Generator 
+# n = 7
+# for i in range (0,n):
+#     print (" "*n , end="")
+#     print("* "*i)
+#     n = n - 1
+
+
+#Nested Loops
+n = 7
+for i in range(0,n):
+    for j in range(0,i+1):
+        print("*", end="")
+    print("")
